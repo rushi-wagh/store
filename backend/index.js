dc6b9f57f211a5245493e840627f3d89c7076c04
Rushi23442 <waghrushi228@gmail.com>
2026-09-08 11:48:45 +0530

    initialized empty project

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("Server is healthy!!");
+});
+
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
